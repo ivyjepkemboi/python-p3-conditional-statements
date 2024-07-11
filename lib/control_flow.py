bb05b9c6d8ae4c0756ffc5106e615c41374e3fcf
Ivy <ivyjkemboi@gmail.com>
2024-07-12 00:23:47 +0300

    conditionals

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,65 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+
+    if username == "admin" and password == "12345":
+        login = "Access granted"
+        return login
+
+    elif username == "ADMIN" and password == "12345":
+        login = "Access granted"
+        return login
+
+    else: 
+        login = "Access denied"
+        return login
+    
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        message = "It's brisk out there!"
+        return message
+
+    elif temperature > 40 and temperature < 65:
+        message = "It's a little chilly out there!"
+        return message
+
+    elif temperature > 85:
+        message = "It's too dang hot out there!"
+        return message
+    
+    else:
+        message = "It's perfect out there!"
+        return message
 
 def fizzbuzz(num):
-    # your code here
-    pass
+
+    if num % 3 == 0 and num % 5 == 0:
+        return ("FizzBuzz")
+    
+    elif num % 3 == 0:
+        return ("Fizz")
+
+    elif num % 5 == 0:
+        return ("Buzz")
+
+    else:
+        return num
+
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+
+    elif operation == "-":
+        return num1-num2
+
+    elif operation == "*":
+        return num1*num2
+
+    elif operation == "/":
+        return num1/num2
+
+    else:
+        print ('Invalid operation!')
+        return None
